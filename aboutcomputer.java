93550b2e5eef6ef138ecdea005b494c16c6a5035
Abhidiviti <abhidiviti19@gmail.com>
2024-07-20 19:11:18 +0530

    Add files via upload

---
+++ aboutcomputer.java
@@ -0,0 +1,7 @@
+//PROGRAM ABOUT COMPUTER
+public class aboutcomputer {
+    
+public static void main(String[] args) {
+    System.out.println(".A computer is an electronic device designed to process, store, and retrieve data.\n It operates based on instructions provided through software applications. \nComputers consist of hardware components such as the CPU, memory, storage devices, and peripherals. \nThey perform a wide range of tasks, from simple calculations to complex simulations. \nThe advent of computers has revolutionized various fields, including science, education, business, and entertainment.\n Computers can be classified into different types, such as desktops, laptops, tablets, and servers. \nThe development of the internet has greatly enhanced the functionality and connectivity of computers. \nThey utilize various operating systems like Windows, macOS, Linux, and others. \nModern computers have advanced capabilities, including artificial intelligence and machine learning.\n They continue to evolve rapidly, driving innovation and transforming our daily lives.");
+}
+}
---
+++ aboutmyself.java
@@ -0,0 +1,7 @@
+//PROGRAM ABOUTMYSELF
+public class aboutmyself {
+   
+public static void main(String[] args) {
+    System.out.println("Hello! I'm a postgraduate student diving into the world of Java programming.\n I have a keen interest in software development and problem-solving. \nMy academic journey includes studying data structures, algorithms, and object-oriented programming. \nI'm currently focusing on mastering Java syntax and its core libraries. \nI enjoy working on small projects to apply what I've learned in class. \nBesides coding, I love exploring new technologies and keeping up with industry trends.\n In my free time, I contribute to open-source projects. \nI also enjoy participating in coding competitions to sharpen my skills.\n My goal is to become a proficient Java developer and contribute to innovative software solutions. \nI'm excited to continue learning and growing in this field.");
+}
+}
